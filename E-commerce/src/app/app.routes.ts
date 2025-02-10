cf8e4d750d4614129264dad03b41aed7f361e970
jamal-aldeen <jamal.aldeen.y@gmail.com>
2025-02-10 10:56:35 +0200

    add Angular projects

--- E-commerce/src/app/app.routes.ts
+++ E-commerce/src/app/app.routes.ts
@@ -2,37 +2,15 @@ import { Routes } from '@angular/router';
 import { ProductsComponent } from './components/products/products.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { ProductCardComponent } from './components/product-card/product-card.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { CartComponent } from './components/cart/cart.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: ProductsComponent,
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  }
-  ,
-  {
-    path: 'register',
-    component: RegisterComponent,
-  }
-  ,
-  {
-    path: 'products-card',
-    component: ProductCardComponent,
-  }
-  ,
-  {
-    path: 'products-details',
-    component: ProductDetailsComponent,
-  }
-  ,
-  {
-    path: 'cart',
-    component: CartComponent,
-  }
+  { path: '', component: ProductsComponent, title: 'Home' },
+  { path: 'login', component: LoginComponent, title: 'Login' },
+  { path: 'register', component: RegisterComponent, title: 'Register' },
+  { path: 'cart', component: CartComponent, title: 'Cart' },
+  { path: 'product/:id', component: ProductDetailsComponent, title: 'Product Details' },
+  { path: '**', component: PageNotFoundComponent, title: '404' }
 ];
